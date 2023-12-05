10c1592f9f420813174bcf612fe8e5f30e751c39
Renee Barros <reneebarros4@gmail.com>
2023-12-04 21:32:52 -0300

    Department entity class

---
+++ src/application/aplication.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class aplication {
+
+	public static void main(String[] args) {
+		
+		
+		Department obj = new Department(1, "BOOKs");
+		System.out.println(obj);
+
+	}
+
+}
