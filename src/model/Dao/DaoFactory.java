8f0ccd8794c888c83a76aa827ffea76560747158
Renee Barros <reneebarros4@gmail.com>
2023-12-07 13:46:54 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/Dao/DaoFactory.java
@@ -0,0 +1,12 @@
+package model.Dao;
+
+import model.Dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+		
+	}
+
+}
---
+++ src/model/Dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,42 @@
+package model.Dao.impl;
+
+import java.util.List;
+
+import model.Dao.SellerDao;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void insert(SellerDao obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(SellerDao obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public SellerDao findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SellerDao> finAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+
+}
