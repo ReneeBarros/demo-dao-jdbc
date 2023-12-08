71f58f77c03adf154de835b466a1faa1da1d5753
Renee Barros <reneebarros4@gmail.com>
2023-12-08 20:02:55 -0300

    Impletment merthod FindByDepartment

--- src/application/aplication.java
+++ src/application/aplication.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.Dao.DaoFactory;
 import model.Dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class aplication {
@@ -14,6 +17,15 @@ public class aplication {
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
+		
+		System.out.println(" ====   TEST 2 method findByDepartment");
+		Department department = new Department(2, null);
+		
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		for ( Seller obj: list) {
+		System.out.println(obj);
+		}
 	}
 
 }
