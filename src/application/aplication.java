aa02fc9d8030b1522bc7126aa538741df41592de
Renee Barros <reneebarros4@gmail.com>
2023-12-08 23:58:01 -0300

    Implements Method Seller UpDate

--- src/application/aplication.java
+++ src/application/aplication.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.Dao.DaoFactory;
@@ -13,12 +14,12 @@ public class aplication {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
-		System.out.println(" ====   TEST 1 findById");
+		System.out.println(" ====   TEST 1 Seller method findById");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
 		
-		System.out.println(" ====   TEST 2 method findByDepartment");
+		System.out.println(" ====   TEST 2 Seller method findByDepartment");
 		Department department = new Department(2, null);
 		
 		List<Seller> list = sellerDao.findByDepartment(department);
@@ -27,13 +28,32 @@ public class aplication {
 		System.out.println(obj);
 		}
 		
-		System.out.println(" ====   TEST 2 method findAll");
+		System.out.println(" ====   TEST 3 Seller method findAll");
 		
 		List<Seller> listFindAll = sellerDao.findAll();
 		
 		for ( Seller findall: listFindAll) {
 		System.out.println(findall);
 		}
+		
+		
+		System.out.println(" ====   TEST 4 Seller method Insert");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id: " + newSeller.getId());
+		
+		
+		System.out.println(" ====   TEST 5 Seller method UpDate");
+
+		seller = sellerDao.findById(1);
+		seller.setName("Renee Barros");
+		seller.setBaseSalary(1000000.00);
+		sellerDao.update(seller);
+		System.out.println("Done!");
+		
+		
+		
 	}
 	
 
--- src/model/Dao/impl/SellerDaoJDBC.java
+++ src/model/Dao/impl/SellerDaoJDBC.java
@@ -25,14 +25,40 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 
 	@Override
-	public void insert(SellerDao obj) {
+	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void update(SellerDao obj) {
-		// TODO Auto-generated method stub
+	public void update(Seller obj) {
+		PreparedStatement st = null;
+
+		try {
+			st = conn.prepareStatement(
+
+					"UPDATE seller "
+					+ "SET Name = ?, Email =?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id =?");
+
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBithDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+
+			st.executeUpdate();
+
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+
+		} finally {
+			DB.closeStatement(st);
+
+		}
 
 	}
 
@@ -92,22 +118,43 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 
 	@Override
-	public List<SellerDao> finAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
 
+		try {
+			st = conn.prepareStatement(
+
+					"INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId ) " + "VALUES "
+							+ "(?,?,?,?,?)",
+					java.sql.Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBithDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+
+			} else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+
+		} finally {
+			DB.closeStatement(st);
+
+		}
 	}
 
-	@Override
-	public void update(Seller obj) {
-		// TODO Auto-generated method stub
-
-	}
 
 	@Override
 	public List<Seller> findAll() {
